e3a354fcf25ec8207ce187f378245260619cb75b
Luis Alfredo Lorenzo <babasbot@gmail.com>
2025-03-17 18:06:36 -0600

    Initial tts module

---
+++ main.py
@@ -0,0 +1,5 @@
+import tts
+
+if __name__ == "__main__":
+    tts.say("Hola culeritos! Soy un furby de barrio.")
+
---
+++ tts.py
@@ -0,0 +1,16 @@
+from espeakng import ESpeakNG
+
+esng_cfg = {
+    "voice": 'es-419',
+    "audio_dev": 0,
+    "pitch": 90,
+    "word_gap": 1,
+    "speed": 180,
+}
+
+_esng = ESpeakNG(**esng_cfg)
+
+def say(text):
+    print("tts:", text)
+    _esng.say(text)
+
